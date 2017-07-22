5f2f3a322da0231cb6d003831aeb97a2584e015a
Dennis Treder <info@dennistreder.de>
2017-07-23 00:10:53 +0200

    deployment works with new win-git :)

--- deploy_to_gh-pages.js
+++ deploy_to_gh-pages.js
@@ -1,7 +1,19 @@
 var ghpages = require('gh-pages');
 var path = require('path');
 
+console.log(
+`
+------------------
+Publishing...`
+);
 ghpages.publish('build', {
   branch: 'gh-pages',
   repo: 'git@github.com:headjump/headjump_website.git'
-}, function(err) {});
+}, function(err) {
+  if(err) {
+    console.log("ERROR: " + err);
+  } else {
+    console.log("DONE :)");
+  }
+  console.log("------------------");
+});
